9aa058df7c6926da65da7a35a8c8816805256c32
Naman Jain <naman.jain@up42.com>
2022-03-28 23:04:09 +0200

    tests initial commit

---
+++ tests/test_dsm2dtm.py
@@ -0,0 +1,73 @@
+import pytest
+
+
+def test_downsample_raster():
+    pass
+
+
+def test_upsample_raster():
+    pass
+
+
+def test_generate_slope_raster():
+    pass
+
+
+def test_get_mean():
+    pass
+
+
+def test_extract_dtm():
+    pass
+
+
+def test_remove_noise():
+    pass
+
+
+def test_array_as_geotiff():
+    pass
+
+
+def test_sdat_to_gtiff():
+    pass
+
+
+def close_gaps():
+    pass
+
+
+def test_smoothen_raster():
+    pass
+
+
+def test_subtract_rasters():
+    pass
+
+
+def test_replace_values():
+    pass
+
+
+def test_expand_holes_in_raster():
+    pass
+
+
+def test_get_raster_crs():
+    pass
+
+
+def test_get_raster_resolution():
+    pass
+
+
+def test_get_res_and_downsample():
+    pass
+
+
+def test_get_updated_params():
+    pass
+
+
+def test_main():
+    pass
